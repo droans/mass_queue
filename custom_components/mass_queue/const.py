7aaedbdc42804eb4b87d0dce687b2f65635d4e0b
Michael Carroll <mjoecarroll@gmail.com>
2025-08-21 09:02:40 -0400

    Reorder Attributes

--- custom_components/mass_queue/const.py
+++ custom_components/mass_queue/const.py
@@ -12,18 +12,17 @@ SERVICE_MOVE_QUEUE_ITEM_DOWN = "move_queue_item_down"
 SERVICE_MOVE_QUEUE_ITEM_NEXT = "move_queue_item_next"
 
 ATTR_CONFIG_ENTRY_ID = "config_entry_id"
-
-ATTR_PLAYER_ENTITY = "entity"
-ATTR_OFFSET = "offset"
 ATTR_LIMIT = "limit"
-ATTR_LIMIT_BEFORE = "limit_before"
 ATTR_LIMIT_AFTER = "limit_after"
+ATTR_LIMIT_BEFORE = "limit_before"
 ATTR_QUEUE_ITEM_ID = "queue_item_id"
 ATTR_MEDIA_TITLE = "media_title"
 ATTR_MEDIA_ALBUM_NAME = "media_album_name"
 ATTR_MEDIA_ARTIST = "media_artist"
 ATTR_MEDIA_CONTENT_ID = "media_content_id"
 ATTR_MEDIA_IMAGE = "media_image"
+ATTR_OFFSET = "offset"
+ATTR_PLAYER_ENTITY = "entity"
 ATTR_QUEUE_ITEMS = "queue_items"
 LOGGER = logging.getLogger(__package__)
 
