00e8de08f57ceb64878860d85f76ddf8f94d7338
Michael Carroll <mjoecarroll@gmail.com>
2025-10-03 17:02:29 -0400

    Linting

--- custom_components/mass_queue/utils.py
+++ custom_components/mass_queue/utils.py
@@ -33,6 +33,7 @@ def get_queue_id_from_player_data(player_data):
         return None
     return current_media.get("queue_id")
 
+
 def return_image_or_none(img_data: dict):
     """Returns None if image is not present or not remotely accessible."""
     if type(img_data) is dict:
@@ -41,6 +42,7 @@ def return_image_or_none(img_data: dict):
         if remote:
             return img
     return None
+
 
 def search_image_list(images: list):
     """Checks through a list of image data and attempts to find an image."""
@@ -52,10 +54,12 @@ def search_image_list(images: list):
             break
     return result
 
+
 def find_image_from_image(data: dict):
     """Attempts to find the image via the image key."""
     img_data = data.get("image")
     return return_image_or_none(img_data)
+
 
 def find_image_from_metadata(data: dict):
     """Attempts to find the image via the metadata key."""
@@ -66,6 +70,7 @@ def find_image_from_metadata(data: dict):
         return None
     return search_image_list(img_data)
 
+
 def find_image_from_album(data: dict):
     """Attempts to find the image via the album key."""
     album = data.get("album", {})
@@ -74,6 +79,7 @@ def find_image_from_album(data: dict):
     if img_data is None:
         return None
     return search_image_list(img_data)
+
 
 def find_image_from_artists(data: dict):
     """Attempts to find the image via the artists key."""
